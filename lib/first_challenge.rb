14119b37fd84809d33c522a50d07d48edd4ad5e3
github username <rasha.baig@gmail.com>
2019-01-14 13:40:33 -0500

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,9 @@
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +14,12 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+contacts.each do |person, data|
+  data.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
+puts  contacts
 end
-
+end
+end
+end
